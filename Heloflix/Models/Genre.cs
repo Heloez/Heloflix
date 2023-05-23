90ef7760a2f76992c3adfde2dd6d2ee853502a38
Heloez <escolaanaehelo@gmail.com>
2023-05-23 08:39:13 -0300

    modificando o models

--- Heloflix/Models/Genre.cs
+++ Heloflix/Models/Genre.cs
@@ -1,6 +1,13 @@
-namespace Heloflix.Models;
+using System.ComponentModel.DataAnnotations;
 
-    public class Genre
-    {
-        
-    }
+namespace ZazaFlix.Models;
+
+public class Genre
+{
+    [Key] //Define a propriedade como Chave Primária. 
+    public byte Id {get; set; }
+
+    [Required] //Requerido - Not Null; Validação.
+    [StringLength(30)] // Tamanho máximo da propriedade.
+    public string Name { get; set; }
+}
